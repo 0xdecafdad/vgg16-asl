ecf2724c0d400ecb34b0495823bd59fac0ebbb1d
0xdecafdad <shaunderstine@gmail.com>
2022-02-28 16:06:07 -0500

    model0.py loads datasets

--- model0.py
+++ model0.py
@@ -3,3 +3,30 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+
+# Data loading and pre-processing
+# Training set
+train_ds = tf.keras.utils.image_dataset_from_directory(
+	"./PetImages",
+	batch_size=32,
+	image_size=(224,224),
+	seed=1337,
+	validation_split=.2,
+	subset="training",
+)
+
+# Validation set
+val_ds = tf.keras.utils.image_dataset_from_directory(
+	"./PetImages",
+	batch_size=32,
+	image_size=(224,224),
+	seed=1337,
+	validation_split=.2,
+	subset="validation",
+)
+
+# Visualize data
+
+# Building model
+
+# Training model
