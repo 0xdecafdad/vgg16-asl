d97104302aff79434762b351a5e8ed678c0d7222
0xdecafdad <shaunderstine@gmail.com>
2022-03-07 02:50:00 -0500

    Create test.py
    Create test.py to test functions from other files

---
+++ test.py
@@ -0,0 +1,11 @@
+# this file is for testing various functions
+from load_data import as_numpy
+
+def main():
+    as_numpy("~/Documents/workspace/vgg16-asl/PetImages/")
+
+    return 0
+# end main()
+
+if __name__=="__main__":
+    main()
