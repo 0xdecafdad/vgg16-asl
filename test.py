2194b4403d77f75e44a39916e3aa973c48ac5745
0xdecafdad <shaunderstine@gmail.com>
2022-03-22 22:36:18 -0400

    Add call to load_data.resize()
    Resize elements of target list using resize function

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 # this file is for testing various functions
 from load_data import as_numpy
+from load_data import resize
 from tensorflow import keras
 from keras.applications.vgg16 import preprocess_input
+import matplotlib.pyplot as plt
 
 def main():
-    #as_numpy("~/Documents/workspace/vgg16-asl/PetImages/")
-    data = as_numpy("./PetImages/", True) # had to add location relative to file
+    # Load images as Numpy arrays
+    data = as_numpy("./PetImages/", True)
 
+    # Preprocess each image
     for elem in data:
         elem = preprocess_input(elem)
+    
+    # Resize each image according to VGG16 constraints
+    resize(data, (224,224))
 
     return 0
 # end main()
